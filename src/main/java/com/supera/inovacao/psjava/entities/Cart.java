153da6f52439ebd36f963997e3c62e606bd7215e
Michel Corrêa <micheloscbc@hotmail.com>
2021-10-09 13:46:23 -0300

    Adicionando Entidades no projeto

---
+++ src/main/java/com/supera/inovacao/psjava/entities/Cart.java
@@ -0,0 +1,23 @@
+package com.supera.inovacao.psjava.entities;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.util.List;
+
+@Entity
+@Data
+public class Cart {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private BigDecimal shippingRate;
+
+    @OneToMany(mappedBy = "cart")
+    @JoinColumn
+    private List<Product> products;
+
+}
---
+++ src/main/java/com/supera/inovacao/psjava/entities/Product.java
@@ -0,0 +1,27 @@
+package com.supera.inovacao.psjava.entities;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Data
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private BigDecimal price;
+
+    private short score;
+
+    private String image;
+
+    @ManyToOne
+    @JoinColumn
+    private Cart cart;
+}
