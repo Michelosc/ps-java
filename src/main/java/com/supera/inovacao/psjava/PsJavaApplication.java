f2437206de23a69285ca239d441c19a6a00aa6b5
Michel Corrêa <micheloscbc@hotmail.com>
2021-10-09 13:26:05 -0300

    Estrutura inicial do projeto

---
+++ src/main/java/com/supera/inovacao/psjava/PsJavaApplication.java
@@ -0,0 +1,13 @@
+package com.supera.inovacao.psjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/supera/inovacao/psjava/PsJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.supera.inovacao.psjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PsJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
