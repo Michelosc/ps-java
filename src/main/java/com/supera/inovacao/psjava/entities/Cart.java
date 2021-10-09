6df4db99dc6bf0615384e84a972ddb307812d208
Michel Corrêa <micheloscbc@hotmail.com>
2021-10-09 16:29:06 -0300

    Adicionando métodos em Cart

--- src/main/java/com/supera/inovacao/psjava/entities/Cart.java
+++ src/main/java/com/supera/inovacao/psjava/entities/Cart.java
@@ -19,4 +19,24 @@ public class Cart {
     @OneToMany(mappedBy = "cart")
     private List<Product> products;
 
+    private BigDecimal subTotal;
+
+    private BigDecimal total;
+
+    public void addProduct(Product product) {
+        products.add(product);
+        addShippingRate();
+    }
+
+    public void addShippingRate() {
+        setShippingRate(new BigDecimal(10));
+    }
+
+    public void calculateTotalShippingRate() {
+        BigDecimal discountValue = new BigDecimal(250);
+        if(shippingRate.compareTo(discountValue) >= 0) {
+            setShippingRate(BigDecimal.ZERO);
+        }
+    }
+
 }
