5d1e39d583c1e0bd7d5323691195cbbe06a60fe1
Michel Corrêa <micheloscbc@hotmail.com>
2021-10-09 22:49:37 -0300

    Adicionando configuração da classe ModelMapper

--- src/main/java/com/supera/inovacao/psjava/services/ProductService.java
+++ src/main/java/com/supera/inovacao/psjava/services/ProductService.java
@@ -1,7 +1,18 @@
 package com.supera.inovacao.psjava.services;
 
+import com.supera.inovacao.psjava.entities.Product;
+import com.supera.inovacao.psjava.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    public Product save(Product product) {
+        return productRepository.save(product);
+    }
+
 }
